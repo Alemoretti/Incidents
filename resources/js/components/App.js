7e775777243ad69f7dabb61edfe0faa4241545fc
Alexandre Moretti <alemoretti2010@hotmail.com>
2021-03-15 05:56:51 -0300

    Updates mix and builds js and css files via webpack (mix)

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render () {
     return (
       <BrowserRouter>
-        <div>asd
+        <div>
           <Header />
           <Switch>
             <Route exact path='/' component={IncidentsList} />
